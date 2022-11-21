ef20b17bd2ee19ff834c361a1c9a444efb404f1c
shimseongbo <seong265@icloud.com>
2022-11-21 15:34:22 +0900

    Initial commit

---
+++ src/main/java/com/example/parametertransaction/ParameterTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.parametertransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParameterTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParameterTransactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parametertransaction/ParameterTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parametertransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParameterTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
